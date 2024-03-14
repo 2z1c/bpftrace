7db31d92ebd89ae9fc0912b036893c32154c60df
edgarzhou <edgarzhou@tinnove.com.cn>
2024-03-14 13:58:37 +0800

    feat: add static Signed-off-by: edgarzhou <edgarzhou@tinnove.com.cn>

--- .github/include/arm64_static.sh
+++ .github/include/arm64_static.sh
@@ -22,19 +22,11 @@ docker run -v $(pwd):$(pwd) -w $(pwd) -i "$IMAGE" <<'EOF'
 set -eux
 BUILD_DIR=build-static
 cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_TESTING=OFF -DSTATIC_LINKING=ON
-make -C "$BUILD_DIR" -j$(nproc)
+make -C "$BUILD_DIR" V=1 CXXFLAGS="-fPIC -static" -j$(nproc) bpftrace
 
 # Basic smoke test
 ./"$BUILD_DIR"/src/bpftrace --help
-
-# Validate that it's a mostly static binary except for libc
-EXPECTED="/lib/ld-musl-x86_64.so.1\nlibc.musl-x86_64.so.1"
-GOT=$(ldd "$BUILD_DIR"/src/bpftrace | awk '{print $1}')
-if ! diff <(echo -e "$EXPECTED") <(echo "$GOT"); then
-  set +x
-  >&2 echo "bpftrace incorrectly linked"
-  exit 1
-fi
+file $(pwd)/build-static/src/bpftrace
 EOF
 
-file $(pwd)/build-static/src/bpftrace
+
